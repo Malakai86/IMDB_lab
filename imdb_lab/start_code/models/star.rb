8b3be760c71c82064e4159e9f2eef6abb620be09
mike <>
2020-03-12 14:51:13 +0000

    added save to stars

--- imdb_lab/start_code/models/star.rb
+++ imdb_lab/start_code/models/star.rb
@@ -1,3 +1,37 @@
+require_relative("../db/sql_runner")
+
 class Star
+
+  attr_reader :id, :first_name, :last_name
+  def initialize(options)
+    @id = options["id"].to_i() if options["id"]
+    @first_name = options["first_name"]
+    @last_name = options["last_name"]
+  end
+
+  def save()
+    sql = "INSERT INTO stars
+    (
+    first_name,
+    last_name
+    )
+    VALUES
+    (
+    $1,$2
+    )
+    RETURNING *"
+    values = [@first_name, @last_name]
+    star = SqlRunner.run(sql, values)[0]
+    @id = star["id"].to_i()
+  end
+
+
+
+
+  def self.map_items(star_data)
+    result = star_data.map{|star| Star.new(star_data)}
+    return result
+  end
+
 
 end
